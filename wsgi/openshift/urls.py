ef3b5cd0855fa7182cd30e7ad2bc0627967c7e1a
A. Bram Neijt <bneijt@gmail.com>
2013-02-24 21:36:39 +0100

    Upload code

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'openshift.views.home', name='home'),
-    # url(r'^openshift/', include('openshift.foo.urls')),
+    url(r'upload/^$', 'openshift.views.upload', name='upload'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from django import forms
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
     file  = forms.FileField()
 
 def handle_uploaded_file(f):
@@ -13,13 +12,15 @@ def handle_uploaded_file(f):
             destination.write(chunk)
 
 def home(request):
+    form = UploadFileForm()
+    return render_to_response('home/home.html', {'form': form})
+
+def upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
             return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render_to_response('home/home.html', {'form': form})
+    return HttpResponseRedirect('/')
 
 
